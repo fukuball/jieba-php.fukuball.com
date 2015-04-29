75122fb54afb10219fb904260461fbb8f424302d
Fukuball Lin <fukuball@gmail.com>
2015-04-29 22:50:54 +0800

    add fukuball link

--- resources/views/jieba-php.blade.php
+++ resources/views/jieba-php.blade.php
@@ -29,11 +29,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Jieba PHP</a>
+          <a class="navbar-brand" href="/">Jieba PHP</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
+            <li class="active"><a href="/">Home</a></li>
+            <li><a href="http://www.fukuball.com">Fukuball</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
