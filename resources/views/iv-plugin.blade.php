4b8e42933049c4e81bb23bdc8eb2f8d03be5b50c
Fukuball Lin <fukuball@gmail.com>
2015-07-31 16:33:40 +0800

    iv plugin

--- resources/views/iv-plugin.blade.php
+++ resources/views/iv-plugin.blade.php
@@ -5,7 +5,7 @@
   js.src = "http://dev.indievox.com/public/javascript/iv-plugin/iv-plugin-sdk.js?version=v0.0.1&appId=j300000039";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'indievox-jssdk'));</script>
-<iframe id="iv_music_purchase_btn_<?php echo $thing_type; ?>_<?php echo $thing_id; ?>" width="500px" height="110px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" src="http://dev.indievox.com/plugin/music-purchase-btn?thing_id=<?php echo $thing_id; ?>&thing_type=<?php echo $thing_type; ?>&layout=default&app_id=j300000039" style="border: none; visibility: visible; width: 500px; height: 110px;"></iframe>
+<iframe id="iv_music_purchase_btn_<?php echo $thing_type; ?>_<?php echo $thing_id; ?>" width="500px" height="115px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" src="http://dev.indievox.com/plugin/music-purchase-btn?thing_id=<?php echo $thing_id; ?>&thing_type=<?php echo $thing_type; ?>&layout=default&app_id=j300000039" style="border: none; visibility: visible; width: 500px; height: 110px;"></iframe>
 <div class="iv-plugin"
      data-thing-type="song"
      data-thing-id="1"
